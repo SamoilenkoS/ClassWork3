07d44fa82c43edc9beae718d9f4e63bd7111f80c
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-11-06 13:05:28 +0200

    Implement tasks for CW 4

--- ConsoleForTasks/Program.cs
+++ ConsoleForTasks/Program.cs
@@ -47,23 +47,78 @@ namespace ConsoleForTasks
             }
         }
 
+        static void MultiplicationTable()
+        {
+            for (int j = 2; j <= 9; j++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{j}*{i}={j * i}");
+                }
+            }
+        }
+
+        static void ASCIITable()
+        {
+            for (int i = 0; i <= 128; i++)
+            {
+                Console.WriteLine($"{i} = {(char)i}");
+            }
+        }
+
+        static void SumFromAToBMultipliedByN()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());//1
+            int b = Convert.ToInt32(Console.ReadLine());//99999
+            int n = Convert.ToInt32(Console.ReadLine());//3
+            //Find sum of numbers from A to B which multiples of N
+            if (n != 0)
+            {
+                int sum = 0;
+                a += (a % n != 0) ? (n - (a % n)) : 0;
+                for (; a <= b; a += n)
+                {
+                    sum += a;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+
+        static void IceCreamCountAndReminder()
+        {
+            double N = Convert.ToDouble(Console.ReadLine());
+            double K = Convert.ToDouble(Console.ReadLine());
+            int count = 0;
+
+            while (N >= K)
+            {
+                N -= K;
+                ++count;
+            }
+
+            Console.WriteLine($"{N} money remain.");
+            Console.WriteLine($"{count} ice creams could be bought.");
+        }
+
         static void Main(string[] args)
         {
-            // >, <, ==, >=, <=, !=
-            // &&, ||, &, |
-            CharType charType = ClassWork3.Task6('c');
-            switch (charType)
+            int userInput;
+            do
             {
-                case CharType.Digit:
-                    Console.WriteLine("Digit");
-                    break;
-                case CharType.Letter:
-                    Console.WriteLine("Letter");
-                    break;
-                case CharType.Other:
-                    Console.WriteLine("Other");
-                    break;
-            }
+                userInput = Convert.ToInt32(Console.ReadLine());
+                switch (userInput)
+                {
+                    case 1:
+                        IceCreamCountAndReminder();
+                        break;
+                    case 2:
+                        SumFromAToBMultipliedByN();
+                        break;
+                    case 0:
+                        break;
+                }
+            } while (userInput != 0);
         }
     }
 }
